4e434c1f89416f46056ec2aadcf2f61ae712dc63
Nakata <4ryo2-meron-math@ezweb.ne.jp>
2019-11-27 03:24:52 +0900

    update akaze fun to akaze class

--- akaze.py
+++ akaze.py
@@ -2,19 +2,27 @@ import cv2
 import pickle
 
 
-def akaze(file,dir="./fig"):
-    img = cv2.imread(f"{dir}/{file}.jpg")
-    img_g = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    akaze = cv2.AKAZE_create()
-    kp= akaze.detect(img_g)
-    img_akaze = cv2.drawKeypoints(img_g, kp, None, flags=4)
-    cv2.imwrite(f"{dir}/{file}_g.png", img_g)
-    cv2.imwrite(f"{dir}/kp/{file}_kp.png", img_akaze)
+class Akaze:
 
-    with open(f"{dir}/points/{file}.csv", "w") as f:
+    def __init__(self, file, out="all", dir="./fig"):
+        self.dir = dir
+        self.out = out
+        self.file = file
+        self.kp = self.calcKp()
+
+    def calcKp(self):
+        img = cv2.imread(f"{self.dir}/{self.file}.png")
+        akaze = cv2.AKAZE_create(threshold=0.0)
+        kp = akaze.detect(img)
+        img_akaze = cv2.drawKeypoints(img, kp, None, flags=4)
+        cv2.imwrite(f"{self.dir}/bef/{self.file}_{self.out}.png", img)
+        cv2.imwrite(f"{self.dir}/kp/{self.file}_kp_{self.out}.png", img_akaze)
+        lst = []
         for p in kp:
-            f.write(f"{p.pt[0]},{p.pt[1]},{p.size}\n")
+            lst.append([p.pt[0], p.pt[1], p.size])
+        return lst
 
-
-if __name__ == '__main__':
-    akaze(file="ff")
+    def output(self):
+        with open(f"{self.dir}/points/{self.file}_{self.out}.csv", "w") as f:
+            for p in self.kp:
+                f.write(f"{p[0]},{p[1]},{p[2]}\n")
---
+++ main.py
@@ -0,0 +1,6 @@
+from akaze import Akaze
+
+if __name__ == "__main__":
+    c = Akaze(file="c++")
+    c = Akaze(file="python")
+    c = Akaze(file="rust")
